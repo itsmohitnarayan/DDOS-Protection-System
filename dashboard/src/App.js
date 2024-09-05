b835565d422e6b1ca955e742a57aaded2b10bd5f
kashyapanjali <anjalikashyap9608@gmail.com>
2024-09-05 12:00:54 +0530

    routing

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -4,15 +4,23 @@ import Main from "./component/Main";
 import DDosDetection from "./component/DDosDetection";
 import DdosMitigation from "./component/DDosMitigation";
 import DDosMonitoring from "./component/DDosMonitoring";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 function App() {
   return (
-    <div className="app">
-      <Navbar />
-      <Main />
-      <DDosDetection />
-      <DdosMitigation />
-      <DDosMonitoring />
-    </div>
+    <Router>
+      <div className="app">
+        <Navbar />
+        {/* Main is rendered always */}
+        <Main />
+        <Routes>
+          <Route path="/" element={<Main />} /> {/* Default route */}
+          <Route path="/ddos-detection" element={<DDosDetection />} />
+          <Route path="/ddos-mitigation" element={<DdosMitigation />} />
+          <Route path="/ddos-monitoring" element={<DDosMonitoring />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- dashboard/src/component/Main.js
+++ dashboard/src/component/Main.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom"; // Import Link from react-router-dom
 import "./Main.css"; // Import the CSS file for styling
 
 function Main() {
@@ -9,27 +10,29 @@ function Main() {
   };
 
   const items = [
-    "Home",
-    "DDosDetection",
-    "DDosMitigation",
-    "Fan Control",
-    "DDosMonitoring",
-    "Game Sync",
-    "App Center",
+    { name: "Home" },
+    { name: "DDosDetection", path: "/ddos-detection" },
+    { name: "DDosMitigation", path: "/ddos-mitigation" },
+    { name: "DDosMonitoring", path: "/ddos-monitoring" },
+    { name: "Fan-Control" },
+    { name: "Game Sync" },
   ];
 
   return (
     <div className="main">
       <ul className="main-list">
         {items.map((item) => (
-          <li key={item} className="main-item">
-            <a
-              href="#"
-              className={`main-link ${activeItem === item ? "active" : ""}`}
-              onClick={() => handleSetActive(item)}
+          <li key={item.name} className="main-item">
+            {/* Use Link instead of <a> for client-side routing */}
+            <Link
+              to={item.path} // Set the path using the Link component
+              className={`main-link ${
+                activeItem === item.name ? "active" : ""
+              }`}
+              onClick={() => handleSetActive(item.name)}
             >
-              {item}
-            </a>
+              {item.name}
+            </Link>
           </li>
         ))}
       </ul>
